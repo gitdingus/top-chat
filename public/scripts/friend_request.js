ad036729ab5d99f2da9397e825834167bd41ad6c
Josh S. <aaabccc87@protonmail.com>
2023-10-28 20:25:06 -0400

    conditionally add listener to friend request form

--- public/scripts/friend_request.js
+++ public/scripts/friend_request.js
@@ -1,20 +1,21 @@
 const friendRequestForm = document.querySelector('#friend-request');
-
-friendRequestForm.addEventListener('submit', function(e) {
-  e.preventDefault();
-  const friendId = friendRequestForm.friendId.value;
-  
-  fetch('/users/me/addfriend', {
-    method: 'post',
-    headers: {
-      'content-type': 'application/json',
-    },
-    body: JSON.stringify({
-      friendId,
-    }),
-  })
-    .then((res) => {
-      console.log(res);
-    });
-  return false;
-});
+if (friendRequestForm !== null) {
+  friendRequestForm.addEventListener('submit', function(e) {
+    e.preventDefault();
+    const friendId = friendRequestForm.friendId.value;
+    
+    fetch('/users/me/addfriend', {
+      method: 'post',
+      headers: {
+        'content-type': 'application/json',
+      },
+      body: JSON.stringify({
+        friendId,
+      }),
+    })
+      .then((res) => {
+        console.log(res);
+      });
+    return false;
+  });
+}
