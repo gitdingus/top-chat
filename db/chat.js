d9eec3ae9a3bef9a40261726ccdd909f8f81a969
Josh S. <aaabccc87@protonmail.com>
2023-10-31 11:46:20 -0400

    functions to create chat and add/remove users to it

---
+++ db/chat.js
@@ -0,0 +1,46 @@
+const Chat = require('../models/chat.js');
+const User = require('../models/user.js');
+
+async function addUserToChat(chatId, userId) {
+  const [ chat, user ] = await Promise.all([
+    Chat.findById(chatId).exec(),
+    User.findById(userId).exec(),
+  ]);
+
+  chat.allowedUsers.push(user);
+
+  return await chat.save();
+}
+
+async function createChat(type, userIds) {
+  const chat = new Chat({
+    type,
+    allowedUsers: userIds,
+  })
+
+  return await chat.save();
+}
+
+async function removeUserFromChat(chatId, userId) {
+  const [ chat, user ] = await Promise.all([
+    Chat.findById(chatId).exec(),
+    User.findById(userId).exec(),
+  ]);
+
+  const userIndex = chat.allowedUsers.findIndex((user) => {
+    if (user._id.toString() === userId) {
+      return true;
+    }
+  });
+
+  if (userIndex !== -1) {
+    chat.allowedUsers.splice(userIndex, 1);
+  }
+
+  return await chat.save();
+}
+module.exports = {
+  addUserToChat,
+  createChat,
+  removeUserFromChat,
+}
