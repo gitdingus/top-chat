ccb6bea43478ba8fdba6ba31a5b1d7e8adb95df4
Josh S. <aaabccc87@protonmail.com>
2023-11-07 15:00:46 -0500

    client side code to display users currently in chat

--- public/scripts/chat.js
+++ public/scripts/chat.js
@@ -2,6 +2,7 @@ const messages = document.querySelector('#messages');
 const messageForm = document.querySelector('#message-form');
 const sendBtn = document.querySelector('#send-message');
 const messageInput = document.querySelector('#message_input');
+const usersList = document.querySelector('#users ul');
 let autoScroll = true;
 let ws;
 
@@ -24,21 +25,55 @@ if (messages != null) {
 if (messageForm !== null) {
   ws = new WebSocket(`ws://localhost:8080/chat/${messageForm.chat_id.value}`);
 
-  ws.addEventListener('message', (messageJSON) => {
-    const message = JSON.parse(messageJSON.data);
-    const messageDiv = createMessage(message);
-    messages.appendChild(messageDiv);
-    if (autoScroll) {
-      messages.scrollTo(0, messages.scrollHeight);
+  ws.addEventListener('message', (packetJSON) => {
+    const packet = JSON.parse(packetJSON.data);
+
+    if (packet.action === 'message') {
+      const message = packet.data;
+      const messageDiv = createMessage(message);
+      messages.appendChild(messageDiv);
+      if (autoScroll) {
+        messages.scrollTo(0, messages.scrollHeight);
+      }
+    }
+
+    if (packet.action === 'current-users') {
+      const currentUsers = packet.data.currentUsers;
+      currentUsers.forEach((user) => {
+        const li = createUserListItem(user);
+        usersList.appendChild(li);
+      });
+    }
+
+    if (packet.action === 'join') {
+      const newUser = packet.data.user;
+      const li = createUserListItem(newUser);
+      usersList.appendChild(li);
+    }
+
+    if (packet.action === 'leave') {
+      const users = usersList.querySelectorAll('li');
+      users.forEach((user) => {
+        if (user.textContent === packet.data.user) {
+          user.remove();
+        }
+      });
     }
   });
 
   ws.addEventListener('close', (code, buf) => {
-    console.log('Connection closing');
+    const disconnectMessage = document.createElement('p');
+    disconnectMessage.textContent = 'You have been disconnected from the server...';
+    messages.appendChild(disconnectMessage);
+    messages.scrollTo(0, messages.scrollHeight);
+    sendBtn.setAttribute('disabled', true);
   })
   
   messageForm.addEventListener('submit', (e) => {
     e.preventDefault();
+    const packet = {};
+
+    packet.action = 'message';
 
     const newMessage = TextMessage({
       chatId: messageForm.chat_id.value,
@@ -46,7 +81,9 @@ if (messageForm !== null) {
       content: messageForm.message_input.value,
     });
 
-    ws.send(JSON.stringify(newMessage));
+    packet.data = newMessage;
+
+    ws.send(JSON.stringify(packet));
     
     messageInput.value = '';
     messageInput.focus();
@@ -77,6 +114,17 @@ function createMessage(message) {
   messageDiv.appendChild(messageP);
 
   return messageDiv;
+}
+
+function createUserListItem(username) {
+  const li = document.createElement('li');
+  const a = document.createElement('a');
+
+  a.href = `/users/${username}`;
+  a.textContent = username;
+
+  li.appendChild(a);
+  return li;
 }
 
 function TextMessage({ chatId, userId, content }) {
