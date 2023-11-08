a77ed3121beda219964c265db7a4ad3b7b89df7f
Josh S. <aaabccc87@protonmail.com>
2023-11-08 18:26:54 -0500

    rename authentication.js to index.js

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -5,6 +5,13 @@ const createAccountDiv = document.querySelector('#create-account-div');
 const loginBtn = document.querySelector('#login-btn');
 const loginDiv = document.querySelector('#login-div');
 
+if (createAccountDiv && loginDiv) {
+  window.addEventListener('load', () => {
+    modals.classList.add('open');
+    loginDiv.classList.add('active');
+  });
+}
+
 exitModalBtns.forEach((btn) => {
   btn.addEventListener('click', () => {
     const activeModal = document.querySelector('#modals .active');
@@ -18,12 +25,22 @@ exitModalBtns.forEach((btn) => {
   });
 });
 
-createAccountBtn.addEventListener('click', function() {
-  modals.classList.add('open');
-  createAccountDiv.classList.add('active');
-});
+if (createAccountBtn) {
+  createAccountBtn.addEventListener('click', function() {
+    modals.classList.add('open');
+    createAccountDiv.classList.add('active');
 
-loginBtn.addEventListener('click', function() {
-  modals.classList.add('open');
-  loginDiv.classList.add('active');
-});
+    // ensure login div is not showing
+    loginDiv.classList.remove('active');
+  });
+}
+
+if (loginBtn) {
+  loginBtn.addEventListener('click', function() {
+    modals.classList.add('open');
+    loginDiv.classList.add('active');
+
+    // ensure create account div is not showing
+    createAccountDiv.classList.remove('active');
+  });
+}
