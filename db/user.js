c13ec0e35e4762051aa635e96df2a5d28d900c1f
Josh S. <aaabccc87@protonmail.com>
2023-11-12 00:59:41 -0500

    acceptFriend: when accepting request attempt to find past chat convo that may exist if users were previously friends

--- db/user.js
+++ db/user.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 const User = require('../models/user.js');
-const { createChat } = require('../db/chat.js');
+const { createChat, findPrivateMessages } = require('../db/chat.js');
 const { generateSaltHash } = require('../utils/passwordUtils.js');
 
 async function acceptFriend(user, friendId) {
@@ -21,7 +21,14 @@ async function acceptFriend(user, friendId) {
 
   await session.withTransaction(async (s) => {
     const friend = await User.findById(friendId).session(s).exec();
-    const chat = await createChat({ type: 'private-message', allowedUsers: [user._id, friend._id] });
+    const prevPrivateMessages = await findPrivateMessages(user, friend);
+
+    if (prevPrivateMessages.length === 0) {
+      const chat = await createChat({ type: 'private-message', allowedUsers: [user._id, friend._id] });
+    } else {
+      chat = prevPrivateMessages[0];
+    }
+
 
     const friendEntry1 = user.friends.find((entry) => {
       return entry.friend._id.toString() === friendId;
