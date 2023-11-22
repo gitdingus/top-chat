72f9f47c1f188776a6510858bc7a18415a1d7c00
Josh S. <aaabccc87@protonmail.com>
2023-11-22 12:40:37 -0500

    use es6 modules, export attachPopupListeners function

--- public/scripts/popup.js
+++ public/scripts/popup.js
@@ -1,42 +1,40 @@
-const popups = document.querySelectorAll('.popup-parent');
+function attachPopupListeners(popupParent) {
+  let hovering = false;
+  const trigger = popupParent.querySelector('.popup-trigger');
+  const popup = popupParent.querySelector('.popup');
+  trigger.addEventListener('pointerover', (pE) => {
+    hovering = true;
+    setTimeout(() => {
+      if (hovering) {
+        popup.style.left = `${pE.clientX}px`;
+        popup.style.top = `${pE.clientY}px`;
 
-if (popups.length > 0) {
-  popups.forEach((popupParent) => {
-    let hovering = false;
-    const trigger = popupParent.querySelector('.popup-trigger');
-    const popup = popupParent.querySelector('.popup');
-    trigger.addEventListener('pointerover', (pE) => {
-      hovering = true;
-      setTimeout(() => {
-        if (hovering) {
-          popup.style.left = `${pE.clientX}px`;
-          popup.style.top = `${pE.clientY}px`;
+        popup.classList.add('active');
+      }
+    }, 300);
+  });
 
-          popup.classList.add('active');
-        }
-      }, 300);
-    });
+  trigger.addEventListener('pointerout', (e) => {
+    hovering = false;
+    setTimeout(() => {
+      if (!hovering) {
+        popup.classList.remove('active');
+      }
+    }, 300);
+  });
 
-    trigger.addEventListener('pointerout', (e) => {
-      hovering = false;
-      setTimeout(() => {
-        if (!hovering) {
-          popup.classList.remove('active');
-        }
-      }, 300);
-    });
+  popup.addEventListener('pointerover', () => {
+    hovering = true;
+  });
 
-    popup.addEventListener('pointerover', () => {
-      hovering = true;
-    });
-
-    popup.addEventListener('pointerout', () => {
-      hovering = false;
-      setTimeout(() => {
-        if (!hovering) {
-          popup.classList.remove('active');
-        }
-      }, 300);
-    });
+  popup.addEventListener('pointerout', () => {
+    hovering = false;
+    setTimeout(() => {
+      if (!hovering) {
+        popup.classList.remove('active');
+      }
+    }, 300);
   });
 }
+
+export default attachPopupListeners;
