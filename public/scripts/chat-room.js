10def6c6b3aa511095bdc7886a4bd7f81fbff11d
Josh S. <aaabccc87@protonmail.com>
2023-12-04 14:22:44 -0500

    move owner options and exit buttons script to chat-room-admin.js

--- public/scripts/chat-room.js
+++ public/scripts/chat-room.js
@@ -5,8 +5,6 @@ const messageForm = document.querySelector('#message-form');
 const sendBtn = document.querySelector('#send-message');
 const messageInput = document.querySelector('#message_input');
 const usersList = document.querySelector('#users ul');
-const ownerOptions = document.querySelector('#owner-options');
-const roomSettings = document.querySelector('#room-settings');
 const setTopicForm = document.querySelector('#set-topic');
 const popupParents = document.querySelectorAll('.popup-parent');
 
@@ -323,20 +321,4 @@ function updateUsersList(list, newList) {
   });
 }
 
-const modals = document.querySelector('#modals');
-const exitBtns = document.querySelectorAll('.exit')
 
-if (ownerOptions) {
-  ownerOptions.addEventListener('click', () => {
-    modals.classList.add('open');
-    roomSettings.classList.add('active');
-  });
-}
-
-exitBtns.forEach((btn) => {
-  btn.addEventListener('click', (e) => {
-    const activeModal = document.querySelector('#modals .active');
-    activeModal.classList.remove('active');
-    modals.classList.remove('open');
-  });
-});
