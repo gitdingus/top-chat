5262cf9876cb665893099d8aa7ce0f7c54ba6458
Josh S. <aaabccc87@protonmail.com>
2024-01-24 13:08:15 -0500

    restructure chat view and modify styling and js

--- public/scripts/chat.js
+++ public/scripts/chat.js
@@ -1,15 +1,40 @@
-const categories = document.querySelectorAll('.category');
+const sectionButtons = document.querySelectorAll('.section-btn');
+const sections = document.querySelectorAll('.section');
+const publicRoomsButton = document.querySelector('#public-rooms-btn');
+const recentRoomsButton = document.querySelector('#recent-rooms-btn');
+const myRoomsButton = document.querySelector('#my-rooms-btn');
+const createChatButton = document.querySelector('#create-chat-btn');
+const publicSection = document.querySelector('#public');
+const recentlyVisitedSection = document.querySelector('#recently-visited');
+const myRoomsSection = document.querySelector('#my');
+const createChatSection = document.querySelector('#create-chat');
 
-if (categories.length > 0) {
-  categories.forEach((category) => {
-    const expandBtn = category.querySelector('button.expand-section');
-
-    expandBtn.addEventListener('click', () => {
-      if (category.classList.contains('expanded')) {
-        category.classList.remove('expanded');
-      } else {
-        category.classList.add('expanded');
-      }
-    });
-  });
+const clearActiveSelection = () => {
+  sectionButtons.forEach((btn) => btn.classList.remove('active'));
+  sections.forEach((section) => section.classList.remove('active'));
 }
+
+publicRoomsButton.addEventListener('click', () => {
+  clearActiveSelection();
+  publicRoomsButton.classList.add('active');
+  publicSection.classList.add('active');
+});
+
+recentRoomsButton.addEventListener('click', () => {
+  clearActiveSelection();
+  recentRoomsButton.classList.add('active');
+  recentlyVisitedSection.classList.add('active');
+});
+
+myRoomsButton.addEventListener('click', () => {
+  clearActiveSelection();
+  myRoomsButton.classList.add('active');
+  myRoomsSection.classList.add('active');
+});
+
+createChatButton.addEventListener('click', () => {
+  clearActiveSelection();
+  createChatButton.classList.add('active');
+  createChatSection.classList.add('active');
+});
+
