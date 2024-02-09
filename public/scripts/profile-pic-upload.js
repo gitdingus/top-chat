aaf8340c65be5615c392aeb26a45fe1eea2ee0ff
Josh S. <aaabccc87@protonmail.com>
2024-02-09 15:39:04 -0500

    create form to upload profile pic, with preview

---
+++ public/scripts/profile-pic-upload.js
@@ -0,0 +1,46 @@
+const uploadImageForm = document.querySelector('#upload-image-form');
+const newProfilePic = document.querySelector('#new-profile-pic');
+const profilePreview = document.querySelector('#preview-image');
+
+if (newProfilePic.files.length > 0) {
+  setProfilePicPreview(newProfilePic.files[0]);
+} else {
+  profilePreview.textContent = 'No image selected';
+}
+
+newProfilePic.addEventListener('change', (e) => {
+  setProfilePicPreview(e.target.files[0]);
+});
+
+uploadImageForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  const form = new FormData(uploadImageForm);
+  console.log(form);
+  fetch(`/users/me/edit/profile-image`, {
+    method: 'post',
+    body: form,
+  })
+    .then((res) => {
+      console.log(res.status);
+      return res.json();
+    })
+    .then((data) => {
+      console.log(data);
+    });
+  return false;
+});
+
+function setProfilePicPreview(file) {
+  let preview = document.createElement('img');
+  let text = document.createElement('p');
+
+  text.textContent = 'Profile pic preview';
+
+  preview.setAttribute('alt', 'profile pic preview');
+  preview.src = URL.createObjectURL(file);
+  
+  profilePreview.textContent = ''; // clears div
+  profilePreview.appendChild(text);
+  profilePreview.appendChild(preview);
+}
